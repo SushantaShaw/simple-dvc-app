f6c60cfa5149576932b222007fc4728b7629ef87
Sushanta Shaw <show.sushanta33@gmail.com>
2024-02-08 20:24:00 +0530

    first commit

---
+++ template.py
@@ -0,0 +1,23 @@
+import os
+dirs = [
+    os.path.join("Data","Raw"),
+    os.path.join("Data","Processed"),
+    "notebooks",
+    "saved_mdoels",
+    "src"
+]
+
+for dir_ in dirs:
+    os.makedirs(dir_,exist_ok=True)
+
+
+files = [
+    "dvc.yaml",
+    "params.yaml",
+    ".gitignore",
+    os.path.join("src","__init__.py")
+]
+
+for file_ in files:
+    with open(file_,"w") as f:
+        pass
