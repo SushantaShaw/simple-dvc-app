433a7f689467ca34e1e582a06ac369570bc2fb5a
Sushanta Shaw <show.sushanta33@gmail.com>
2024-02-10 12:33:08 +0530

    added all the prediction

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from src.get_data import read_params
+import pandas as pd
+from prediction import api_response
+
+params = read_params("params.yaml")
+app = Flask(__name__)
+
+# Load the trained machine learning model
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get data from the request
+    data = request.json
+    # Perform prediction using the loaded model
+    if request.json:
+        response = api_response(request.json)
+        return jsonify({"prediction": response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ prediction.py
@@ -0,0 +1,75 @@
+from src.get_data import read_params
+import pandas as pd
+import joblib
+import json
+
+class ColumnNotFound(Exception):
+    def __init__(self,message="column not found"):
+        self.message = message
+        super().__init__(self.message)
+
+class NotInRange(Exception):
+    def __init__(self,message="Values are not in range"):
+        self.message = message
+        super().__init__(self.message)
+
+class MissingColumns(Exception):
+    def __init__(self,message="Some columns are missing"):
+        self.message = message
+        super().__init__(self.message)
+
+def predict(data):
+    params = read_params("params.yaml")
+    model_dir_path = params["webapp_model_dir"]
+    data = pd.DataFrame(data,index=[0])
+    model = joblib.load(model_dir_path)
+    out = model.predict(data)[0]
+    try:
+        if 3<=out<=8:
+            return out
+        else:
+            raise NotInRange
+    except NotInRange:
+        return "Unexpected result"
+    
+def get_schema(schema_path="notebooks/schema.json"):
+    with open(schema_path) as json_file:
+        schema = json.load(json_file)
+    return schema
+
+
+def validation(data):
+    def _column_validation(col):
+        schema = get_schema()
+        if col not in list(schema.keys()):
+            raise ColumnNotFound
+    def _column_missing_check(data=data):
+        schema = get_schema()
+        for i in schema.keys():
+            if i not in data.keys():
+                raise MissingColumns
+
+    for col in data.keys():
+        _column_validation(col)
+
+    _column_missing_check()
+    return True
+
+def api_response(data):
+    try:
+        if validation(data):
+            response = predict(data)
+            return response
+    except ColumnNotFound as e:
+        response = {"the_exected_cols": ','.join(list(get_schema().keys())), "response": str(e) }
+        return response
+    except MissingColumns as e:
+        response = {"the_exected_cols": ','.join(list(get_schema().keys())), "response": str(e) }
+        return response
+
+if __name__=="__main__":
+    schema = get_schema()
+    schema.popitem()
+    print(api_response(schema))
+
+
